385c9fca2e52bb06cf5efdb488400bea63eb886d
Devin <devin@devinschulz.com>
2014-08-01 21:08:47 -0400

    Fix Indentation and Spaces

--- gulp.sh
+++ gulp.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-if [ ! gem spec sass > /dev/null 2>&1 && ! gem spec scss-lint > /dev/null 2>&1 ];
+if [ ! gem spec sass > /dev/null 2 > &1 && ! gem spec scss-lint > /dev/null 2 > &1 ];
   then
     echo "Installing Ruby Gems"
     bundle install
@@ -10,13 +10,13 @@ fi
 
 if [ ! -d node_modules ];
   then
-  echo "Installing Dependencies"
+    echo "Installing Dependencies"
     npm install
 fi
 
 if [ ! -d vendors ];
   then
-  echo "Installing Bower"
+    echo "Installing Bower"
     npm install bower
     bower install
 fi
